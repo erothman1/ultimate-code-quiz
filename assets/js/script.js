cbf1f35a64a8eec66095d94915115e0eb66ddb39
Elva Rothman <erothman@tulane.edu>
2023-03-25 17:45:02 -0400

    gave start page display:none when start button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,24 +4,35 @@ var startQuiz = document.getElementById("start")
 //Create timer variable
 var timer = document.getElementById("time")
 
+//Create variable for starting page
+var startPage = document.getElementById("start-page")
+
 //Create variable for starting seconds left 
 var secondsLeft = 75 
 
+//Puts timer content on screen 
 timer.textContent = secondsLeft
 
 //setInterval function that holds countdown
 function setTime() {
 
+    //Hides start page 
+    startPage.setAttribute("style", "display: none")
+
+    //Sets interval
     var timerInterval = setInterval(function() {
         secondsLeft --
         timer.textContent = secondsLeft
 
         if (secondsLeft === 0) {
+            //Stops execution of action at set interval
             clearInterval(timerInterval)
         }
 
     }, 1000)
+
+
 }
 //Listen for click event on start button
-//Clicking start button will initiate the countdown and take user to quiz page one 
+//Clicking start button will initiate the countdown, hide start page, and take user to quiz page one 
 startQuiz.addEventListener("click", setTime)
